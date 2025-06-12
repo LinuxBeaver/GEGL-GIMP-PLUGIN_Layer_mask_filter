ae4d136bb6b391c22812d94605b6bde22831e0cc
LinuxBeaver <jaydc@gmx.us>
2025-06-12 17:51:20 -0400

    removing pippins leftover 2006 copyright

--- SourceCode/quick_mask_filter/quickmaskfilter.c
+++ SourceCode/quick_mask_filter/quickmaskfilter.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with GEGL; if not, see <https://www.gnu.org/licenses/>.
  *
- * Credit to Øvind Kolas (pippin) for major GEGL contributions
+ * Credit to Øyvind Kolås (pippin) for major GEGL contributions
  * 2024 Beaver Layer Mask Filter 
 
 This filter was inspired by "Refine Edges" in Photoshop CS5 and Mr Q's live preview selection plugin on Gimp chat 
